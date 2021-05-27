affa702846c8e44afaec9baf991c06c9dfef9c99
martinobdl <martino.n.bernasconi@gmail.com>
2021-05-27 12:15:13 +0200

    small changes

--- README.md
+++ README.md
@@ -5,11 +5,3 @@ script to convert .tex pictures (tikz) into pdfs
 ```
 python3 standalone.py -i picture.tex -o /directory/output
 ```
-
-## unused labels
-script to find unused label in a tex document
-
-### Usage
-```
-python3 unusedlabel.py -d main/directory
-```
--- standalone.py
+++ standalone.py
@@ -31,8 +31,12 @@ basename = os.path.splitext(os.path.basename(args.i.name))[0]
 outfile = os.path.join(args.o, basename+'.pdf')
 
 if __name__ == "__main__":
-    subprocess.run(['pdflatex', '-output-directory=/tmp', '-jobname=foo', '/tmp/tmp.tex', ],
+    subprocess.run(['pdflatex', '--enable-write18', '--extra-mem-top=100000000',
+                    '--synctex=1', '-output-directory=/tmp', '-jobname=foo',
+                    '/tmp/tmp.tex', ],
                    check=True, text=True)
 
     subprocess.run(['mv', '/tmp/foo.pdf', outfile],
                    check=True, text=True)
+
+    print("Saved in {}".format(outfile))
